2e198a0b896ca6943c5d07357ff8eb2e79c275b6
Leon <anthonygarayleon@gmail.com>
2022-08-27 11:22:10 -0300

    rutas

--- public/js/index.js
+++ public/js/index.js
@@ -11,19 +11,19 @@ $menu = qs( '#menu' );
 
 let name = prompt( 'Ingrese su nombre' );
    
-name === "" ? $h2.innerHTML = 'invitado' : $h2.innerHTML = name;
+name === "" ? $h2.innerHTML += 'invitado' : $h2.innerHTML += name;
 
 $h2.style.textTransform= 'uppercase';
 $a.style.color = '#E51B3E';
 
 let opt =  confirm('¿Desea colocar un fonde de pantalla?');
 
-opt && ( $body.classList.add += 'fondo' );
+opt &&  $body.classList.add( 'fondo' );
 
 $p = document.querySelectorAll( 'p' );
 
 for (let i = 0; i < $p.length; i++) {
-    $p[i] % 2 === 0 ? $p.classList.add = 'destacadoPar' : $p.classList.add = 'descadoImpar';
+    $p[i] % 2 === 0 ? $p.classList.add('destacadoPar') : $p.classList.add('descadoImpar');
 };
 
 $main.style.display= 'block';
